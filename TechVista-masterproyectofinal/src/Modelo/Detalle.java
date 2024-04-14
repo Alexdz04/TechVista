2d714d84855c179fb593aecfae54e46fb0ee1294
Lean-AZ <164468109+Lean-AZ@users.noreply.github.com>
2024-04-14 14:23:02 -0400

    Detalle

---
+++ TechVista-masterproyectofinal/src/Modelo/Detalle.java
@@ -0,0 +1,71 @@
+package Modelo;
+
+
+
+// Clase que representa un detalle de venta 
+
+public class Detalle {
+    private int id;
+    private String cod_pro;
+    private int cantidad;
+    private double precio;
+    private int id_venta;
+
+    // Constructor por defecto
+    public Detalle() {
+    }
+
+    // Constructor con parámetros
+    public Detalle(int id, String cod_pro, int cantidad, double precio, int id_venta) {
+        this.id = id;
+        this.cod_pro = cod_pro;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.id_venta = id_venta;
+    }
+
+    // Métodos get y set para id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Métodos get y set para cod_pro
+    public String getCod_pro() {
+        return cod_pro;
+    }
+
+    public void setCod_pro(String cod_pro) {
+        this.cod_pro = cod_pro;
+    }
+
+    // Métodos get y set para cantidad
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    // Métodos get y set para precio
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // Métodos get y set para id_venta
+    public int getId_venta() {
+        return id_venta;
+    }
+
+    public void setId_venta(int id_venta) {
+        this.id_venta = id_venta;
+    }
+}
